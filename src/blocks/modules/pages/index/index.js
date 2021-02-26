1bb2ea0a27c2b44371e16df3e17a9901a82c09dc
aprinciple <avprinciple@gmail.com>
2021-02-26 23:18:56 +0200

    Added v.2.0

--- src/blocks/modules/pages/index/index.js
+++ src/blocks/modules/pages/index/index.js
@@ -1,4 +1,5 @@
 const homeSlider = document.querySelector('.home-slider');
+const offerSlider = document.querySelector('.p-home__offer-slider');
 if (homeSlider) {
   let currentSlide = homeSlider.querySelector('.home-slider__count-current');
   let totalSlides = homeSlider.querySelector('.home-slider__count-total');
@@ -19,4 +20,37 @@ if (homeSlider) {
   slider.events.on('transitionEnd', function (info, eventName) {
     currentSlide.textContent = `0${slider.getInfo().displayIndex}`;
   });
+}
+
+if (offerSlider) {
+  const prev = offerSlider.querySelector('.p-home__offer-slider-button--prev');
+  const next = offerSlider.querySelector('.p-home__offer-slider-button--next');
+  let slider = tns({
+    'container': offerSlider.querySelector('.p-home__offer-slides'),
+    'items': 1,
+    'responsive': {
+      576: {
+        'gutter': 10,
+        'items': 2
+      },
+      768: {
+        'gutter': 5,
+        'items': 3
+      },
+      992: {
+        'gutter': 15,
+      },
+      1200: {
+        'items': 1,
+        'gutter': 0,
+      }
+    },
+    'nav': false,
+    'swipeAngle': false,
+    'controls': false,
+    'preventScrollOnTouch': 'force'
+  });
+
+  prev.addEventListener('click', (e) => slider.goTo('prev'));
+  next.addEventListener('click', (e) => slider.goTo('next'));
 }
