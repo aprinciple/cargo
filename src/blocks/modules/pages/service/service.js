6b42623fc7cf4d0ae22b87cd314570a7e81d57c7
aprinciple <avprinciple@gmail.com>
2021-03-11 22:47:46 +0200

    Added popup cargo-list

--- src/blocks/modules/pages/service/service.js
+++ src/blocks/modules/pages/service/service.js
@@ -13,3 +13,18 @@ if (itemsTypesCargo) {
     });
   } );
 }
+
+const showListCargo = document.querySelectorAll('.show-list-cargo');
+if (showListCargo) {
+  const list = document.querySelector('.types-cargo__list-cargo');
+  const close = list.querySelector('.types-cargo__list-cargo-close');
+
+  close.addEventListener('click', () => list.style.display = 'none');
+
+  showListCargo.forEach((item) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      list.style.display = 'block';
+    });
+  });
+}
